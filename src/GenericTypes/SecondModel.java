dac1583fc4c9584bb8881e542826e2bf51ce11a5
AjayVyas68 <ajayvyas625@gmail.com>
2022-12-05 17:46:12 +0530

    update Generice in git

---
+++ src/GenericTypes/SecondModel.java
@@ -0,0 +1,22 @@
+package GenericTypes;
+
+public class SecondModel {
+    private String name;
+    private String first;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+}
